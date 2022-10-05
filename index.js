752639b312fca659ffba3d70c927a9a165c27e7e
luofei <1063477160@qq.com>
2022-10-06 00:17:21 +0800

    吸附

--- index.js
+++ index.js
@@ -1,20 +1,25 @@
 (() => {
   const idioms = ["诗情画意", "南来北往", "一团和气", "落花流水"];
-  let charCollection = [];
-  let charAreas=[];
   let charCellGroup = document.querySelector(".char-cell-group");
-  let oChars=null;
-  let startX=0;
-  let startY=0;
-  let cellX=0;
-  let cellY=0;
-  let mouseX=0;
-  let mouseY=0;
+  let oBlanks = document.querySelectorAll(".blank-cell-group .wrapper");
+  let charCollection = [];
+  let charAreas = [];
+  let blankAreas = [];
+  let resArr = [];
+  let oChars = null;
+  let startX = 0;
+  let startY = 0;
+  let cellX = 0;
+  let cellY = 0;
+  let mouseX = 0;
+  let mouseY = 0;
   const init = () => {
     charCollection = formatCharsArr();
     render();
-    oChars=charCellGroup.querySelectorAll('.cell-item .wrapper')
-    getAreas(oChars,charAreas);
+    oChars = charCellGroup.querySelectorAll(".cell-item .wrapper");
+    getAreas(oBlanks, blankAreas);
+    console.log("blankAreas", blankAreas);
+    getAreas(oChars, charAreas);
     bindEvent();
   };
   function render() {
@@ -24,56 +29,82 @@
     });
     charCellGroup.innerHTML = str;
   }
-  function bindEvent(){
-    for(let i=0;i<oChars.length;i++){
-        oChars[i].addEventListener('touchstart',handleTouchStart,false)
-        oChars[i].addEventListener('touchmove',handleTouchMove,false)
-        oChars[i].addEventListener('touchend',handleTouchEnd,false)
+  function bindEvent() {
+    for (let i = 0; i < oChars.length; i++) {
+      oChars[i].addEventListener("touchstart", handleTouchStart, false);
+      oChars[i].addEventListener("touchmove", handleTouchMove, false);
+      oChars[i].addEventListener("touchend", handleTouchEnd, false);
     }
   }
-  function handleTouchStart(e){
-    cellW=this.offsetWidth;
-    cellH=this.offsetHeight;
-    cellX=this.offsetLeft;
-    cellY=this.offsetTop;
-    startX=e.touches[0].clientX;
-    startY=e.touches[0].clientY;
-    mouseX=startX-cellX;
-    mouseY=startY-cellY;
-    this.style.width=cellW+'px';
-    this.style.height=cellH+'px'
-    this.style.position='fixed';
-    this.style.left=cellX+'px'
-    this.style.top=cellY+'px'
+  function handleTouchStart(e) {
+    cellW = this.offsetWidth;
+    cellH = this.offsetHeight;
+    cellX = this.offsetLeft;
+    cellY = this.offsetTop;
+    startX = e.touches[0].clientX;
+    startY = e.touches[0].clientY;
+    mouseX = startX - cellX;
+    mouseY = startY - cellY;
+    this.style.width = cellW + "px";
+    this.style.height = cellH + "px";
+    this.style.position = "fixed";
+    this.style.left = cellX + "px";
+    this.style.top = cellY + "px";
   }
-  function handleTouchMove(e){
+  function handleTouchMove(e) {
     e.preventDefault();
-    const moveX=e.touches[0].clientX;
-    const moveY=e.touches[0].clientY;
-    cellY=moveY-mouseY;
-    cellX=moveX-mouseX;
-    this.style.left=cellX+'px'
-    this.style.top=cellY+'px'
+    const moveX = e.touches[0].clientX;
+    const moveY = e.touches[0].clientY;
+    cellY = moveY - mouseY;
+    cellX = moveX - mouseX;
+    this.style.left = cellX + "px";
+    this.style.top = cellY + "px";
   }
-  function handleTouchEnd(e){
-      const _index=Number(this.dataset.index)
-      const charArea=charAreas[_index];
-      this.style.left=charArea.startX+'px';
-      this.style.top=charArea.startY+'px'
-  }
-  function getAreas(domCollection,arrWrapper){
-    let startX=0;
-    let startY=0;
-    for(let i=0;i<domCollection.length;i++){
-       startX= domCollection[i].offsetLeft;
-       startY= domCollection[i].offsetTop;
-       arrWrapper.push({
-        startX,
-        startY
-       })
+  function handleTouchEnd(e) {
+    const blankWidth = oBlanks[0].offsetWidth;
+    const blankHeight = oBlanks[0].offsetHeight;
+    for (let i = 0; i < blankAreas.length; i++) {
+      if (resArr[i] !== undefined) {
+        continue;
+      }
+
+      let { startX, startY } = blankAreas[i];
+      if (
+        (cellX > startX &&
+          cellX < startX + blankWidth / 2 &&
+          cellY > startY &&
+          cellY < startY + blankHeight / 2) ||
+        (cellX + blankWidth > startX + blankWidth / 2 &&
+          cellX + blankWidth < startX + blankWidth &&
+          cellY > startY &&
+          cellY < startY + blankHeight / 2)
+      ) {
+        setPosition(this,{startX,startY})
+        return;
+      }
     }
 
-
+    // 回弹逻辑
+    const _index = Number(this.dataset.index);
+    const charArea = charAreas[_index];
+    this.style.left = charArea.startX + "px";
+    this.style.top = charArea.startY + "px";
+  }
+  function setPosition(el, { startX, startY }) {
+    el.style.left = startX + "px";
+    el.style.top = startY + "px ";
+  }
+  function getAreas(domCollection, arrWrapper) {
+    let startX = 0;
+    let startY = 0;
+    for (let i = 0; i < domCollection.length; i++) {
+      startX = domCollection[i].offsetLeft;
+      startY = domCollection[i].offsetTop;
+      arrWrapper.push({
+        startX,
+        startY,
+      });
+    }
   }
   function formatCharsArr() {
     let _arr = [];
